865829064402dc16836bec52dcf81d6d6483ba4c
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-08-10 22:59:13 -0400

    feat: implements the button theme.

--- src/components/atoms/ButtonDefaultSmall/index.tsx
+++ src/components/atoms/ButtonDefaultSmall/index.tsx
@@ -1,15 +1,34 @@
 import style from './style.module.css';
-import type { ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 
 interface iconOption {
   children: ReactNode;
   typeButton: 'error' | 'default';
 }
 
+type AvailableTheme = 'dark' | 'light';
+
 const ButtonDefaultSmall = ({ typeButton, children }: iconOption) => {
+  const [theme, setTheme] = useState<AvailableTheme>('dark');
+
+  function handleTheme(event) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <section className={`${style.buttonDefault} ${style[typeButton]}`}>
-      <a href='#'>{children}</a>
+      <a href='#' onClick={handleTheme}>
+        {children}
+      </a>
     </section>
   );
 };
